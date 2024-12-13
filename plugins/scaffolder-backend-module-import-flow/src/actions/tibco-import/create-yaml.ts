83950252c85e3c387c3353953ea522e750f3da04
sgantaya <sgantaya@tibco.com>
2024-12-13 15:24:18 +0530

    Resolved prettier issues.

--- plugins/scaffolder-backend-module-import-flow/src/actions/tibco-import/create-yaml.ts
+++ plugins/scaffolder-backend-module-import-flow/src/actions/tibco-import/create-yaml.ts
@@ -15,28 +15,28 @@ export function createYamlAction() {
   }>({
     id: 'tibco:create-yaml',
     description:
-        'Tibco platform create yaml action, refer to examples for outputStructure schema',
+      'Tibco platform create yaml action, refer to examples for outputStructure schema',
     examples,
     schema: {
       input: z.object({
         failOnError: z
-            .boolean()
-            .optional()
-            .describe(
-                'Boolean flag to stop the task when there is an error, optional, default is false, when true task execution will be stopped in this step when there is an error',
-            ),
+          .boolean()
+          .optional()
+          .describe(
+            'Boolean flag to stop the task when there is an error, optional, default is false, when true task execution will be stopped in this step when there is an error',
+          ),
         sourcePath: z
-            .string()
-            .optional()
-            .describe(
-                'Source path relative to workspace, optional, path within the workspace that will be used as the repository root',
-            ),
+          .string()
+          .optional()
+          .describe(
+            'Source path relative to workspace, optional, path within the workspace that will be used as the repository root',
+          ),
         outputFile: z
-            .string()
-            .optional()
-            .describe(
-                'The name of the output yaml file, optional, default is catalog-info.yaml',
-            ),
+          .string()
+          .optional()
+          .describe(
+            'The name of the output yaml file, optional, default is catalog-info.yaml',
+          ),
         outputStructure: z.record(z.union([z.string(), z.number(), z.any()]), {
           description: 'Output structure',
         }),
@@ -44,24 +44,24 @@ export function createYamlAction() {
     },
     async handler(ctx) {
       ctx.logger.info(
-          `Source path relative to workspace: ${ctx.input.sourcePath || ''}`,
+        `Source path relative to workspace: ${ctx.input.sourcePath || ''}`,
       );
       ctx.logger.info(
-          `Fail on error: ${ctx.input.failOnError ? 'True' : 'False'}`,
+        `Fail on error: ${ctx.input.failOnError ? 'True' : 'False'}`,
       );
       const outFileName = ctx.input.outputFile || 'catalog-info.yaml';
       ctx.logger.info(`Output File name: ${outFileName}`);
       ctx.logger.info(`Output Structure: `, ctx.input.outputStructure);
       const relativeWorkspacePath = resolveSafeChildPath(
-          ctx.workspacePath,
-          ctx.input.sourcePath || '',
+        ctx.workspacePath,
+        ctx.input.sourcePath || '',
       );
       ctx.logger.info(`Final workspace path: ${relativeWorkspacePath}`);
       try {
         const yamlData = dump(ctx.input.outputStructure);
         const filePath = resolveSafeChildPath(
-            relativeWorkspacePath,
-            outFileName,
+          relativeWorkspacePath,
+          outFileName,
         );
         await promises.writeFile(filePath, yamlData, 'utf8');
         ctx.logger.info(`Created Yaml file with name: ${outFileName}`);
