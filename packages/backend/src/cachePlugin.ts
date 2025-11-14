23970f5dcdaaec52a370995a6e7f879a9da9bd8e
subrat gantayat <sgantaya@tibco.com>
2025-11-15 00:06:59 +0530

    Fixed keyv connect issue with postgresql db.

--- packages/backend/src/cachePlugin.ts
+++ packages/backend/src/cachePlugin.ts
@@ -2,16 +2,25 @@
  * Copyright (c) 2023-2025. Cloud Software Group, Inc. All Rights Reserved. Confidential & Proprietary
  */
 
-import { createBackendPlugin } from '@backstage/backend-plugin-api';
+import {
+  coreServices,
+  createBackendPlugin,
+} from '@backstage/backend-plugin-api';
 import { KeyvStore } from './cacheService.ts';
+import { Knex } from 'knex';
+
+const PLUGIN_ID: string = 'tibco_hub_cache';
 
 export default createBackendPlugin({
   pluginId: 'tibco_hub_cache',
   register(env) {
     env.registerInit({
-      deps: {},
-      async init() {
-        KeyvStore.initialize();
+      deps: {
+        database: coreServices.database,
+      },
+      async init({ database }) {
+        const knex: Knex = await database.getClient();
+        KeyvStore.initialize(knex, PLUGIN_ID);
         await KeyvStore.keyv.clear();
       },
     });
--- packages/backend/src/cacheService.ts
+++ packages/backend/src/cacheService.ts
@@ -4,35 +4,37 @@
 
 import Keyv from 'keyv';
 import KeyvPostgres from '@keyv/postgres';
-
-const POSTGRES_HOST = process.env.POSTGRES_HOST;
-const POSTGRES_PORT = process.env.POSTGRES_PORT;
-const POSTGRES_USER = process.env.POSTGRES_USER;
-const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
-const POSTGRES_DB =
-  process.env.POSTGRES_DB || 'backstage_plugin_tibco_hub_cache';
+import { Knex } from 'knex';
 
 export class KeyvStore {
   public static keyv: Keyv;
   public static keyvMemory: Keyv;
-  public static initialize() {
-    if (
-      !(
-        POSTGRES_HOST &&
-        POSTGRES_PORT &&
-        POSTGRES_USER &&
-        POSTGRES_PASSWORD &&
-        POSTGRES_DB
-      )
-    ) {
-      throw new Error('Missing POSTGRES configurations required for Keyv');
+  public static initialize(knex: Knex, PLUGIN_ID: string) {
+    if (knex.client.driverName === 'pg') {
+      const cfg = knex.client.config.connection;
+      const connectionString =
+        typeof cfg === 'string'
+          ? cfg
+          : `postgres://${cfg.user}:${cfg.password}@${cfg.host}:${
+              cfg.port ?? 5432
+            }${cfg.database ? `/${cfg.database}` : ''}`;
+      let schema = 'public';
+      if (typeof cfg !== 'string' && !cfg.database) {
+        schema = PLUGIN_ID;
+      }
+      KeyvStore.keyv = new Keyv(
+        new KeyvPostgres({
+          uri: connectionString,
+          schema,
+        }),
+      );
+    } else {
+      KeyvStore.keyv = new Keyv();
     }
-    KeyvStore.keyv = new Keyv(
-      new KeyvPostgres(
-        `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`,
-      ),
-    );
     KeyvStore.keyvMemory = new Keyv();
+    KeyvStore.keyv.on('error', err => {
+      throw new Error(`Error while initializing KeyvStore ${err as Error}`);
+    });
     KeyvStore.keyv.on('error', err => {
       throw new Error(`Error while initializing KeyvStore ${err as Error}`);
     });
